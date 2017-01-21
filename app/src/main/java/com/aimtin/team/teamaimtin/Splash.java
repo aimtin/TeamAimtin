b2e1c04c9d38d6e75117d01fc2a9fcb63041c476
aimtin <chanduuu@gmail.com>
2017-01-22 00:43:01 +0530

    Initial commit

---
+++ app/src/main/java/com/aimtin/team/teamaimtin/Splash.java
@@ -0,0 +1,116 @@
+package com.aimtin.team.teamaimtin;
+
+/**
+ * Created by Chandu on 30-12-2016.
+ */
+
+/*
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+@SuppressLint("NewApi")
+public class Splash extends Activity {
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+        Thread t =new Thread(){
+            public void run(){
+                try{
+                    sleep(10000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i =new Intent(Splash.this,MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        t.start();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}*/
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class Splash extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splash.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splash.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splash.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
+}
